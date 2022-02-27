47b483927646e2a3c76f6c469bd3599fd74dc8a6
ursueugen <ursu_eugen@hotmail.com>
2022-02-27 15:15:28 +0200

    integrate with MongoDB

--- stack/stack/pipelines.py
+++ stack/stack/pipelines.py
@@ -5,9 +5,35 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
 from itemadapter import ItemAdapter
 
 
-class StackPipeline:
+class MongoPipeline:
+
+    def __init__(self, mongo_uri, mongo_db, mongo_username, mongo_password):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.mongo_username = mongo_username
+        self.mongo_password = mongo_password
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items'),
+            mongo_username=crawler.settings.get('MONGO_USERNAME'),
+            mongo_password=crawler.settings.get('MONGO_PASSWORD')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(f"mongodb://{self.mongo_username}:{self.mongo_password}@{self.mongo_uri}")
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        collection_name = spider.name
+        self.db[collection_name].insert_one(ItemAdapter(item).asdict())
         return item
--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -62,9 +62,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'stack.pipelines.StackPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'stack.pipelines.MongoPipeline': 300,
+}
+MONGO_URI = "localhost"
+MONGO_DATABASE = "scrape"
+MONGO_USERNAME = "root"
+MONGO_PASSWORD = "example"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
